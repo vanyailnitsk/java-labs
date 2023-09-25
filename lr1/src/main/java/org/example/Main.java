3f034c0a19bc5c9191f97df5a674b040ab1fcc7e
Vanya Ilnitskiy <vanyailnitsk@yandex.ru>
2023-09-25 18:40:45 +0300

    LR1 with comments

---
+++ lr1/src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        // Scanner - это штука, которая считывает данные с потока (System.in - консоль)
+        Scanner in = new Scanner(System.in);
+        System.out.print("x = ");
+        double x = in.nextDouble(); // Например считать double
+        System.out.print("y = ");
+        double y = in.nextDouble();
+        System.out.print("d = ");
+        double d = in.nextDouble();
+        double numerator = Math.pow(Math.cos(y),3)+Math.pow(2,x)*d;
+        double denominator = Math.pow(Math.E,y)+Math.log(Math.pow(Math.sin(x),2)+7.4);
+        double result = numerator/denominator;
+        System.out.println("Result is: "+(int)result);
+        int alphabetNumber = (int) (result*9); // 9 - номер варианта, на который надо умножить
+        System.out.println("Number in alphabet: "+alphabetNumber);
+        String str = String.valueOf(alphabetNumber); // Приведение числа к строке
+        String italianAlphabet = "ABCDEFGHILMNOPQRSTUVZ"; // Тут должен быть алфавит по варианту
+        PrintWriter pw = new PrintWriter("output.txt"); // PrintWriter записывает в файл
+        for (char c : str.toCharArray()) { // цикл по символам строки
+            char letter = italianAlphabet.charAt((int)c-'0'); // Тут берется буква по номеру в алфавите
+            System.out.print(letter+" ");
+            pw.print(letter);
+        }
+        pw.close();
+    }
+}
